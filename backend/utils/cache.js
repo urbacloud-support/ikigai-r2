337949413c4cb30d55d3886001a4b621ebb3e161
WarPigC <callaniruddhji@gmail.com>
2026-08-07 22:57:30 +0530

    Wipe old ikigai2 implementation

--- backend/utils/cache.js
+++
@@ -1,6 +0,0 @@
-import NodeCache from 'node-cache';
-
-// Initialize cache with standard TTL of 1 hour (3600 seconds)
-const cache = new NodeCache({ stdTTL: 3600, checkperiod: 600 });
-
-export default cache;
